64e2231c5c345bc6c61ae5d9d95c3b77ab7342eb
Z151 <292700563@qq.com>
2021-08-25 14:48:20 +0800

    https://spring.io/guides/gs/accessing-data-rest/

--- src/main/java/com/erayt/web01/repository/Person03Repository.java
+++ src/main/java/com/erayt/web01/repository/Person03Repository.java
@@ -3,6 +3,8 @@ package com.erayt.web01.repository;
 import com.erayt.web01.domain.Person;
 import com.erayt.web01.domain.Person03;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
@@ -10,9 +12,13 @@ import java.util.List;
  * @Auther: Z151
  * @Date: 2021/8/24 16:12
  */
+//没理解，需要再看
+    //path: 访问地址：
+
+@RepositoryRestResource(collectionResourceRel = "people03", path = "people13")
 public interface Person03Repository extends PagingAndSortingRepository<Person03,Long> {
 
-    List<Person03> findByLastName(String name);
+    List<Person03> findByLastName(@Param("name") String name);
 
 }
 
